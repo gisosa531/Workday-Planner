552796f8cca80df09b87d2ea5f2588fb8a4e0445
gisosa <gio53196@gmail.com>
2021-07-13 18:02:33 -0700

    Added function that checks time by adding classes and animated a notification text as well as added more comments and css styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Loads HTML and CSS
+// Loads HTML and CSS for jquery
 $(document).ready(function () {
    
    
@@ -11,6 +11,18 @@ $(document).ready(function () {
         var time = $(this).parent().attr("id");
         //stores items in localStorage
         localStorage.setItem(time, text);
+
+        //Notification that item was saved to localstorage by adding class to end
+        $('.notification').addClass('working');
+        setTimeout (function () {
+            $('.notification').removeClass('working');
+          }, 9000);
+        
+          $(".saveBtn").animate({
+            width: '300px',
+          }, 1000);
+          $(".notification").show().animate({opacity: '1'}, 3000);
+
     });
 
     // values for saved data stored in localstorage
@@ -25,7 +37,38 @@ $(document).ready(function () {
     $("#time16 .textDescription").val(localStorage.getItem("time16"));
     $("#time17 .textDescription").val(localStorage.getItem("time17"));
 
+    //Function that checks time and adapts to css file
+    function hourChecker() {
+        //stores current number of hours
+        var currentTime = moment().hour();
+        //loops each time block
+        $(".timeblock").each(function () {
+            // converts string to integers
+            var blockTime = parseInt($(this).attr("id").split("time")[1]);
+            console.log( blockTime, currentTime)
+
+            //Analyzes time and adds or removes class attribute depending on time.
+            if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else if (blockTime < currentTime) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
     //Displays date in the format specified
     $("#currentDay").html(moment().format("MMMM Do YYYY, h:mm:ss a"));
-        
+    
+    
+    hourChecker();
 })
